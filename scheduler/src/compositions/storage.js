d3d9b19ffada6ba0ab350a3fc6041bd27162976e
N3N <thsehgml37@korea.ac.kr>
2022-06-17 16:01:10 +0900

    Comment: add desc about storage (localStorage)

--- scheduler/src/compositions/storage.js
+++ scheduler/src/compositions/storage.js
@@ -1,7 +1,9 @@
 // localstorage에 데이터를 저장하는 코드
-import {reactive, toRefs} from 'vue'
+import {reactive, toRefs} from 'vue' //변수에 반응성을 더해주기위해 사용
+//reactive는  객체에 반응성을 더해주는 함수
+//toRefs는 객체으 ㅣ내부 속성들 모두에게 반응성을 더해준다
 
-export const useStorage = () => {
+export const useStorage = () => {   //useStorage 함수를 만들고 이 함수를 ES모듈로 export시켜준다
     const KEY = 'my-todo-list'
     const storage_obj = reactive({storage_id: 0})
     const loadTodos = (initTodos) =>{
