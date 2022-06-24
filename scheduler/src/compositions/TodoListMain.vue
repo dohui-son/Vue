88704664e9e975ba692eb764f4e27c2372656bf7
N3N <thsehgml37@korea.ac.kr>
2022-06-24 12:39:00 +0900

    Feat: TodoListNew - 신규할일 추가 기능

--- scheduler/src/compositions/TodoListMain.vue
+++ scheduler/src/compositions/TodoListMain.vue
@@ -1,91 +1,104 @@
 <!-- TodoListMain컴포넌트 기획 : TodoListMenu로부터 filters의 키값을 받아, 연계된 함수 호출후 요구된 배열을 TodoList에 전달하는 역할-->
 <template>
   <todo-list-menu v-on:change-filter="onChangeFilter" class="p-0">
-
+    <div v-for="key in Object.keys(filered_todos)" :key="key" class="mb-3">
+      <div v-if="use_category">
+        <em>{{ key }}</em>
+      </div>
+      <todo-list :data="filtered_todos[key]" />
+    </div>
+    <div class="my-2 mt-5">
+      <span style="background-color: red">&nbsp; </span>&nbsp;
+      <strong>처리하지 못한 작업</strong>
+    </div>
+    <todo-list :data="pending_todos" />
   </todo-list-menu>
 </template>
 
 <script>
-import  {ref, provide, inject, watch} from 'vue'
-import  {useFilter} from "./useFilter";
+import { ref, provide, inject, watch } from "vue";
+import { useFilter } from "./useFilter";
 
 import TodoListMenu from "../components/TodoListMenu";
 import TodoList from "../components/TodoList";
 
 export default {
-  name:'TodoListMain',
-  setup(props){
+  name: "TodoListMain",
+  setup(props) {
     const {
       getPendingTodos,
-        getActiveTodayTodos,
-        getCompletedTodayTodos,
-        getAllTodayTodos,
-        getAllTodos,
-    } = useFilter()
-    const filter  = ref(0)
-    const filtered_todos = ref([])
-    const pending_todos = ref([])
-    const use_category = ref(false)
-    const todos = inject('todos')
+      getActiveTodayTodos,
+      getCompletedTodayTodos,
+      getAllTodayTodos,
+      getAllTodos,
+    } = useFilter();
+    const filter = ref(0);
+    const filtered_todos = ref([]);
+    const pending_todos = ref([]);
+    const use_category = ref(false);
+    const todos = inject("todos");
 
     const filters = {
-      0:{
-        str: 'PLANS',
+      0: {
+        str: "PLANS",
         func: getActiveTodayTodos,
-        category:false,
+        category: false,
       },
-      1:{
-        str:'COMPLETE',
+      1: {
+        str: "COMPLETE",
         func: getCompletedTodayTodos,
         category: false,
       },
-      2:{
-        str: 'TODAY LISTS',
+      2: {
+        str: "TODAY LISTS",
         func: getAllTodayTodos,
-        category:false,
+        category: false,
       },
-      3:{
-        str:'WHOLE PLANS'
+      3: {
+        str: "WHOLE PLANS",
         func: getAllTodos,
-        category:true
+        category: true,
       },
-    }
+    };
 
-    provide('filters', filters)
+    provide("filters", filters);
 
-    const groupBy = (todos) =>{
-      return todos.reduce((acc, cur)=>{
-        acc[cur['date']] = acc[cur['date']] || []
-        acc[cur['date']].push(cur)
-        return acc
-      }, {})
-    }
+    const groupBy = (todos) => {
+      return todos.reduce((acc, cur) => {
+        acc[cur["date"]] = acc[cur["date"]] || [];
+        acc[cur["date"]].push(cur);
+        return acc;
+      }, {});
+    };
 
-    const onChangeFilter = (filter_idx) =>{
-      filter.value = Number(filter_idx)
-    }
+    const onChangeFilter = (filter_idx) => {
+      filter.value = Number(filter_idx);
+    };
 
     watch(
-        [()=>
-        filter.value, todos.value],
-        ([new_filter,new_todos], [old_filter, old_todos]) =>{
-          pending_todos.value = getPendingTodos(todos)
-          if(typeof new_filter != 'undefined'){
-            let temp_todos = filters[new_filter].func(todos)
-            filtered_todos.value = groupBy(temp_todos)
-            use_category.value = filters[new_filter].category
-          }
-        },
-        {immediate: true}
-
-
-        return {
-          filter, pending_todos,filtered_todos,use_category,onChangeFilter,
+      [() => filter.value, todos.value],
+      ([new_filter, new_todos], [old_filter, old_todos]) => {
+        pending_todos.value = getPendingTodos(todos);
+        if (typeof new_filter != "undefined") {
+          let temp_todos = filters[new_filter].func(todos);
+          filtered_todos.value = groupBy(temp_todos);
+          use_category.value = filters[new_filter].category;
         }
+      },
+      { immediate: true }
+    );
+
+    return {
+      filter,
+      pending_todos,
+      filtered_todos,
+      use_category,
+      onChangeFilter,
+    };
   },
-  components:{
+  components: {
     TodoList,
-    TodoListMenu
+    TodoListMenu,
   },
-}
+};
 </script>
---
+++ scheduler/src/compositions/TodoListNew.vue
@@ -0,0 +1,41 @@
+<!-- 신규 할일 추가 - 유저에게서 새 할일을 입력받음 -->
+<template>
+  <section class="mb-5">
+    <div class="container">
+      <div class="row justify-content-center m-2">
+        <div class="col border border-primary rounded">
+          Todo : 현재 작업 진행중----------------------여기서부터 다시 진행
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { reactive, toRefs, inject, ref } from "vue";
+export default {
+  name: "TodoListNew",
+  setup() {
+    const today = inject("Today");
+    const addTodo = inject("addTodo");
+    const val_obj = reactive({
+      job: "",
+      date: today,
+      today: today,
+    });
+
+    const onAddTodo = () => {
+      if (val_obj.job.length > 0) {
+        addTodo(val_obj.job, val_obj.date);
+        val_obj.job = "";
+        val_obj.date = today;
+      }
+    };
+
+    return {
+      ...toRefs(val_obj),
+      onAddTodo,
+    };
+  },
+};
+</script>
