fb796ccde4ba7fe80f6d0c29f6df6939f6b44672
N3N <thsehgml37@korea.ac.kr>
2022-06-24 18:01:01 +0900

    Feat: add axios

---
+++ sum-project/sum/src/modules/axios.js
@@ -0,0 +1,22 @@
+import { axios } from "axios";
+
+export default function () {
+  const BASE_URL = "http://localhost:8000";
+  const axiosGet = (URL, onSuccess = null, onFailed = null) => {
+    const final_URL = URL.startsWith("http") ? URL : BASE_URL + URL;
+    axios.get(final_URL).them((resp) => {
+      if (resp.status === 200 && resp.data.rsp === "ok") {
+        if (onSuccess) {
+          onSuccess(resp.data);
+        }
+      } else {
+        if (onFailed) {
+          onFailed(resp.data);
+        }
+      }
+    });
+  };
+  return {
+    axiosGet,
+  };
+}
