791ee7eab1cd81960958e41de8b5a3eecc5f44fc
N3N <thsehgml37@korea.ac.kr>
2022-06-24 14:24:11 +0900

    Feat: add navbar

--- sum/src/App.vue
+++ sum/src/App.vue
@@ -1,14 +1,14 @@
-<template><div></div></template>
+<template>
+  <NavBarName />
+</template>
 
 <script>
+/* eslint-disable */
+import NavBarName from "./components/NavBar.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    NavBarName,
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-</style>
